f40fc30bcc28f5f3d3e799cd0438581253d5e577
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2024-04-13 12:48:08 -0300

    feat(depencies_module): creating dependencies module and providing depedencies for IpService

--- my_weather/lib/main.dart
+++ my_weather/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:my_weather/services/dependencies_module.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
+  setupDependencies();
 }
 
 class MyApp extends StatelessWidget {
--- my_weather/lib/services/ip.service.dart
+++ my_weather/lib/services/ip.service.dart
@@ -1,13 +1,23 @@
 import 'package:http/http.dart' as http;
+import 'package:get_it/get_it.dart';
 
 class IpService {
+  final http.Client _httpClient;
+
+  IpService(this._httpClient);
+
   Future<String> getUserIpAddress() async {
-    final response = await http.get(Uri.parse('https://api.ipify.org?format=json'));
-    if (response.statusCode == 200) {
-      final ipAddress = response.body;
-      return ipAddress;
-    } else {
-      throw Exception('Failed to fetch IP address');
+    try {
+      final response =
+          await _httpClient.get(Uri.parse('https://api.ipify.org?format=json'));
+      if (response.statusCode == 200) {
+        final ipAddress = response.body;
+        return ipAddress;
+      } else {
+        throw Exception('Failed to fetch IP address');
+      }
+    } catch (e) {
+      throw Exception('Error fetching IP address: $e');
     }
   }
 }
